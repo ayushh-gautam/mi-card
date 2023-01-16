3d917e224c7f58fd30806340426e332cae49827c
ayushh-gautam <ayushgautam896@gmail.com>
2023-01-16 22:14:52 +0545

    Mi Custom Card

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'mainPage.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: MainPage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
+
+
+//45
