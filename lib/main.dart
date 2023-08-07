e45df4109681d9c55c77cb6b691f4011f121ec68
ayushh-gautam <ayushgautam896@gmail.com>
2023-08-07 23:41:09 +0545

    did some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,43 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
-import 'mainPage.dart';
-
-void main() {
-  runApp(
-    const MaterialApp(
-      home: MainPage(),
-      debugShowCheckedModeBanner: false,
-    ),
-  );
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: MainPage(),
+  ));
 }
 
+class Contacts {
+  final String name;
+  Contacts({
+    required this.name,
+  });
+}
 
-//45
+class ContactBook {
+  ContactBook._sharedInstance();
+  static final ContactBook _shared = ContactBook._sharedInstance();
+  factory ContactBook() => _shared;
+
+  final List<Contacts> _contacts = [];
+  int get length => _contacts.length;
+
+  void addContact({required Contacts contact}) {
+    _contacts.add(contact);
+  }
+
+  void removeContact({required Contacts contact}) {
+    _contacts.remove(contact);
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
